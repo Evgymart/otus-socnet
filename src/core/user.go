5e61e5f7bdb0765afbd1fb039d92b5a1cc2b9735
Evgymart <alignant@yandex.ru>
2023-03-14 08:13:19 +0300

    Add password hashing

--- src/core/user.go
+++ src/core/user.go
@@ -3,9 +3,12 @@ package core
 import (
 	"otus/socnet/db"
 	"otus/socnet/models"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func AddUser(user *models.User) Message {
+	hashPassword(user)
 	database := db.GetDatabase()
 	err := db.AddUser(database, user)
 	if err != nil {
@@ -19,4 +22,12 @@ func AddUser(user *models.User) Message {
 		Status:  "OK",
 		Message: "",
 	}
+}
+
+func hashPassword(user *models.User) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
+	if err != nil {
+		panic("Could not hash a password")
+	}
+	user.Password = string(hashed)
 }
